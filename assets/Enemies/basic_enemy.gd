83bc968385672e8b33e2fb8ee3107037e98cec7a
VeryShinyPants <117368680+VeryShinyPants@users.noreply.github.com>
2026-04-26 22:03:15 +0200

    prep for enemies
    Tilføjet diverse stubbe til enemies og projectiles, stadigt work in progress

---
+++ assets/Enemies/basic_enemy.gd
@@ -0,0 +1,21 @@
+
+#class for basic enemies.
+#this is a template for the first enemy types
+class_name EnemyBasic
+
+# variable for the health of the enemy
+var enemyHP = 25
+
+
+
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
