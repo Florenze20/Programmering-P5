4a87e2a85646f7762957a9abf7cfe59ae5d18137
VeryShinyPants <117368680+VeryShinyPants@users.noreply.github.com>
2026-04-28 09:54:12 +0200

    comment on enemy script
    added comments for functionality of variables in the enemy class code

--- assets/Enemies/basic_enemy.gd
+++ assets/Enemies/basic_enemy.gd
@@ -5,7 +5,13 @@ class_name EnemyBasic
 
 # variable for the health of the enemy
 var enemyHP = 25
+# variable for enemy tier. Higher tier means stronger enemy
+# tier 0 is a straight forward melee enemy with a small health total.
 var enemyTier = 0
+# variable for the enemy's size. size 2 is the standard size
+# size 1 is a small unit, with sizes 3 and 4 being bigger units.
+# this may be a variable that is checked to determine footprint 
+#rather than the size itsels
 var enemyFootprint = 2
 
 
