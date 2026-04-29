0cbabd060f39ce63990e4e2e73a975ae7a2b3316
Florenze20 <gerzonflorenze@gmail.com>
2026-04-29 12:43:54 +0200

    s
    s

--- script/basic_enemy.gd
+++ script/basic_enemy.gd
@@ -1,19 +1,26 @@
-extends CharacterBody3D
-#class_name EnemyBasic
 
-# Stats
+#class for basic enemies.
+#this is a template for the first enemy types
+class_name EnemyBasic
+
+# variable for the health of the enemy
 var enemyHP = 25
+# variable for enemy tier. Higher tier means stronger enemy
+# tier 0 is a straight forward melee enemy with a small health total.
 var enemyTier = 0
+# variable for the enemy's size. size 2 is the standard size
+# size 1 is a small unit, with sizes 3 and 4 being bigger units.
+# this may be a variable that is checked to determine footprint 
+#rather than the size itsels
 var enemyFootprint = 2
 
 
-@export var speed := 2.0
-var lane_x := 0.0
 
-func _ready():
-	position.x = lane_x
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
 
-func _physics_process(delta):
-	position.x = lane_x
-	velocity = Vector3(0, 0, -speed)
-	move_and_slide()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
