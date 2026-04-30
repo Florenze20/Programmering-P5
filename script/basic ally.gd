148d3ff167be2b1ddfbee897c9720526dd3fd0c9
VeryShinyPants <117368680+VeryShinyPants@users.noreply.github.com>
2026-04-30 14:16:14 +0200

    Ally class code
    Added more to Hp for Allies

--- script/basic ally.gd
+++ script/basic ally.gd
@@ -2,8 +2,16 @@ extends Node
 
 class_name BasicAlly
 
-var AllyHp = 10
+# Ally Max hp and current hp respectively
+var AllyMaxHp = 20
+var AllyHp = 20
+var AllyCost = 100
+
+func AllyLifeLoss(int):
+	AllyHp -= int
+
 # Called when the node enters the scene tree for the first time.
+
 func _ready() -> void:
 	pass # Replace with function body.
 
