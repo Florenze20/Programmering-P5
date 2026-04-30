ae1f8fa5616219615dfa23ef8376c29f192daebc
VeryShinyPants <117368680+VeryShinyPants@users.noreply.github.com>
2026-04-30 14:35:13 +0200

    Basic Ally update

--- script/basic ally.gd
+++ script/basic ally.gd
@@ -6,16 +6,23 @@ class_name BasicAlly
 var AllyMaxHp = 20
 var AllyHp = 20
 var AllyCost = 100
+var AllyBaseDamage= 5
 
-func AllyLifeLoss(int):
-	AllyHp -= int
+#Damage taking function, replace AllyBaseDamage with a enemy damage variable or constant
+func AllyLifeLoss(AllyBaseDamage):
+	AllyHp -= AllyBaseDamage
+	if AllyHp < 1: 
+		AllyHp = 0
+
+
+	
 
 # Called when the node enters the scene tree for the first time.
-
 func _ready() -> void:
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
+@warning_ignore("unused_parameter")
 func _process(delta: float) -> void:
 	pass
