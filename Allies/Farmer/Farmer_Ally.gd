ab98585c6e3667d292030a0865a9aaefae55d9db
VeryShinyPants <117368680+VeryShinyPants@users.noreply.github.com>
2026-05-05 00:15:35 +0200

    Added Gunner and Farmer Ally Classes

---
+++ Allies/Farmer/Farmer_Ally.gd
@@ -0,0 +1,21 @@
+extends Node
+
+
+var AllyMaxHp = 50
+var AllyHp = 50
+var AllyCost = 50
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+#There is currently no currency. This needs to add to the currency total.
+#Can alternatively be an ofshoot of "func shoot():" with another projectile
+func farm():
+	pass
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
---
+++ Allies/Gunner/Gunner_Ally.gd
@@ -0,0 +1,13 @@
+extends BasicAlly
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	super._ready()
+	var AllyDRFlat = 5
+
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
