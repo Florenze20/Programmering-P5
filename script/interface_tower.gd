0ad167494dd1b754e2f379e26cf230685ad91f19
Florenze20 <gerzonflorenze@gmail.com>
2026-05-04 12:16:02 +0200

    inetface tower
    får den til at holde op med at svæve når den har musen på den

--- script/interface_tower.gd
+++ script/interface_tower.gd
@@ -2,10 +2,24 @@ extends Node2D
 
 var time = 0
 var start_y = 0
-#Svæve animation til BoxINterface til towers
+var mouse_on_interface = false
+
+# Svæve animation til BoxInterface til towers
 func _ready():
 	start_y = $BoxInterface.position.y
 
+#stopper animationen når musen er på interface
 func _process(delta):
+	var mouse_pos = get_global_mouse_position()
+	var box_rect = Rect2(
+		$BoxInterface.global_position,
+		$BoxInterface.size
+	)
+
+	mouse_on_interface = box_rect.has_point(mouse_pos)
+
+	if mouse_on_interface:
+		return
+
 	time += delta
 	$BoxInterface.position.y = start_y + sin(time * 2) * 10
