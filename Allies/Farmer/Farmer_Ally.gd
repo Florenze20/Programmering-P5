5f36bb85c1d91508a28f88bf42f5b32904e501a2
VeryShinyPants <117368680+VeryShinyPants@users.noreply.github.com>
2026-05-06 08:15:31 +0200

    changed money variable

--- Allies/Farmer/Farmer_Ally.gd
+++ Allies/Farmer/Farmer_Ally.gd
@@ -1,8 +1,5 @@
 extends BasicAlly
 
-
-
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	super._ready()
@@ -14,7 +11,7 @@ func _ready():
 #There is currently no currency. This needs to add to the currency total.
 #Can alternatively be an ofshoot of "func shoot():" with another projectile
 func farm():
-	var money += 50
+	var money =+ 50
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
